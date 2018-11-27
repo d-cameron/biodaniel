4ce51df6763b15be6109bc6b0ceb5155bbbca01c
Ubuntu <ubuntu@None.novalocal>
2018-11-27 03:51:20 +0000

    Added code coverge to CI build

--- .travis/unit-test.sh
+++ .travis/unit-test.sh
@@ -4,7 +4,7 @@ set -e
 errors=0
 
 # Run unit tests
-python biodaniel/biodaniel_test.py || {
+coverage run biodaniel/biodaniel_test.py || {
     echo "'python python/biodaniel/biodaniel_test.py' failed"
     let errors+=1
 }
